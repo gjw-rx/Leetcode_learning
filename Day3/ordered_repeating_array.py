2a20166fa49f4e5b2f656052a019506b13b2b726
Gjw <1798955210@qq.com>
2025-03-17 22:04:44 +0800

    乐扣26题解

---
+++ Day3/ordered_repeating_array.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow=0
+        fast=0
+        while(fast<len(nums)):
+            if(nums[fast]!=nums[slow]):
+                slow+=1
+                nums[slow]=nums[fast]
+            else:
+                fast+=1
+        return slow+1
