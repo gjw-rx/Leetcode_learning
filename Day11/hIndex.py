b1abfb8f0df91a32bfdb15110f2b94a38e272f50
Gjw <1798955210@qq.com>
2025-04-21 12:43:22 +0800

    Revert "力扣380"
    This reverts commit 01ca6961945a417733638c282c188e7c34dfef48.

--- Day11/hIndex.py
+++
@@ -1,8 +0,0 @@
-from typing import List  
-class Solution:
-    def hIndex(self, citations: List[int]) -> int:
-        max_h = 0
-        # 基础思路 每次循环 循环得到结果再与其他的值进行比对 用max存
-        for i in range(1,len(citations)):
-            if(citations[i]>max_h):
-                
